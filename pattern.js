6633ecb76e1e5e8b4f6ebe286564bca9fa7db972
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-12-03 08:16:17 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -17,4 +17,58 @@ O/P
    ***
     *
 
-  Inverted Triangke:
+SQUARE:
+function square(rows) {
+    console.log("square");
+    for (let i = 1; i <= rows; i++) {
+        let space = "".repeat(rows);
+        let stars = "*".repeat(rows)
+        console.log(space+stars); 
+    }
+    console.log("\n");
+}
+square(3);
+O/P
+***
+***
+***
+
+Hollow Square:
+function HollowSquare(rows) {
+    console.log("square");
+    for (let i = 1; i <= rows; i++) {
+        let row;
+        if (i === 1 || i === rows) {
+            row = "*".repeat(rows);
+        } else {
+            row = "*" + " ".repeat(rows - 2) + "*";
+        }
+        console.log(row);
+    }
+    console.log("\n");
+}
+HollowSquare(5);
+
+O/P:
+*****
+*   *
+*   *
+*   *
+*****
+
+Inverted Triangle:
+function InvertedTriangle(rows){
+    console.log("InvertedTriangle")
+    
+    for(let i = rows; i>= 1;i--){
+        console.log("*".repeat(i))
+    }
+}
+InvertedTriangle(5)
+O/P
+*****
+****
+***
+**
+*
+
